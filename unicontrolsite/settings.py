fdf5859c9fcd458eeed63597ce969462831ea34c
Artaey <lyngeeske@hotmail.com>
2024-11-19 10:58:42 +0100

    postgres setup

--- unicontrolsite/settings.py
+++ unicontrolsite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'unicontrolsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'POST': '5432',
     }
 }
 
