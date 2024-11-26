4eca7a3c3538af93f302344225027f4e2936134d
Artaey <lyngeeske@hotmail.com>
2024-11-26 13:33:24 +0100

    db setup + admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import SyncSparepart, SyncBomList, DropdownField, DropdownOption, User, Save, Order
 
 # Register your models here.
+class SyncSparepartAdmin(admin.ModelAdmin):
+    list_display = ('no', 'name', 'itemDescription', 'price') 
+    search_fields = ('name', 'itemDescription')
+admin.site.register(SyncSparepart, SyncSparepartAdmin)
+
+class SyncBomListAdmin(admin.ModelAdmin):
+    list_display = ('no', 'itemDescription', 'quantity', 'parentItemNo')
+admin.site.register(SyncBomList, SyncBomListAdmin)
+
+class DropdownFieldAdmin(admin.ModelAdmin):
+    list_display = ('lableText', 'dropdownRequired', 'readMore')
+admin.site.register(DropdownField, DropdownFieldAdmin)
+
+class DropdownOptionAdmin(admin.ModelAdmin):
+    list_display = ('fieldId', 'itemNo', 'optionText')
+admin.site.register(DropdownOption, DropdownOptionAdmin)
+
+admin.site.register(User)
+
+class SaveAdmin(admin.ModelAdmin):
+    list_display = ('userId', 'fieldId', 'optionId', 'name', 'isFavorite')
+admin.site.register(Save, SaveAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('userId', 'itemNo', 'orderNumber', 'price')
+admin.site.register(Order, OrderAdmin)
--- order/models.py
+++ order/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+class SyncSparepart(models.Model):
+    no = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=25)
+    itemDescription = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.no}"
+
+class SyncBomList(models.Model):
+    parentItemNo = models.ForeignKey(SyncSparepart, on_delete=models.CASCADE, to_field="no")
+    no = models.IntegerField()
+    itemDescription = models.CharField(max_length=255)
+    quantity = models.SmallIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.no}"
+
+class DropdownField(models.Model):
+    lableText = models.CharField(max_length=255)
+    dropdownRequired = models.BooleanField()
+    readMore = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.lableText
+
+class DropdownOption(models.Model):
+    fieldId = models.ForeignKey(DropdownField, on_delete=models.CASCADE)
+    itemNo = models.ForeignKey(SyncSparepart, on_delete=models.CASCADE, to_field="no")
+    optionText = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.fieldId} + {self.itemNo}"
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Save(models.Model):
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    fieldId = models.ForeignKey(DropdownField, on_delete=models.DO_NOTHING)
+    optionId = models.ForeignKey(DropdownOption, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=255)
+    isFavorite = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Order(models.Model):
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    itemNo =  models.ForeignKey(SyncSparepart, on_delete=models.CASCADE, to_field="no")
+    orderNumber = models.IntegerField()
+    price = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return f"{self.orderNumber}"
