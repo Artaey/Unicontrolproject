9ccfd1b62a301ed1ef3aba3100f40d2a471674ec
Artaey <lyngeeske@hotmail.com>
2024-12-04 11:05:59 +0100

    first iteration database intergration

--- order/views.py
+++ order/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SyncSparepart, DropdownField, DropdownOption
 
 # Create your views here.
 def index(request):
     return render(request, "order/index.html")
 
 def excavator(request):
-    return render(request, "order/excavator.html")
+    categories = DropdownField.objects.prefetch_related('dropdownoption_set')
+    data = []
+    for category in categories:
+        options = category.dropdownoption_set.all()
+        data.append({
+            "category": category,
+            "options": options,
+        })
+    return render(request, "order/excavator.html", {"data": data})
 
 def summary(request):
     return render(request, "order/summary.html")
